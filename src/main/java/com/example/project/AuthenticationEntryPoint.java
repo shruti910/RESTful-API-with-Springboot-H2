5e80f1be7807102bcfe474a761672f99f075cb62
shruti910 <shruti.lovelyyy@gmail.com>
2020-10-06 22:29:45 +0530

    added springboot security basic auth and config files

--- src/main/java/com/example/project/AuthenticationEntryPoint.java
+++ src/main/java/com/example/project/AuthenticationEntryPoint.java
@@ -1,28 +1,40 @@
-//package com.example.project;
-//
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
-//
-//	@Override
-//    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
-//      throws IOException, ServletException {
-//
-//    }
-//	
-//	@Override
-//    public void afterPropertiesSet() throws Exception {
-//
-//    }
-//
-//}
+package com.example.project;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+//We can extend the BasicAuthenticationEntryPoint to intercept the error message before it is sent to the client.
+//This way we can control what we sent as a response.
+
+@Component
+public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+
+        @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
+        response.setContentType("application/json");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("Custom HTTP Status 401 : " + authException.getMessage());
+
+    }
+	
+	@Override
+    public void afterPropertiesSet() throws Exception {
+        setRealmName("ShrutiAPI");
+        super.afterPropertiesSet();
+    }
+
+}
--- src/main/java/com/example/project/SpringSecurityConfig.java
+++ src/main/java/com/example/project/SpringSecurityConfig.java
@@ -1,12 +1,44 @@
-//package com.example.project;
-//
-//
-//
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//
-//public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//
-//
-//}
+package com.example.project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	 @Autowired
+    private AuthenticationEntryPoint myauthenticationEntryPoint;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		 http
+          .authorizeRequests()
+          .anyRequest()
+          .authenticated()
+          .and()
+          .httpBasic()
+          .realmName(myauthenticationEntryPoint.getRealmName())
+          .authenticationEntryPoint(myauthenticationEntryPoint);
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		
+	
+    	auth
+          .inMemoryAuthentication()
+          .withUser("user")
+          .password("password")
+          .roles("USER")
+          .and()
+          .withUser("admin")
+          .password("admin")
+          .roles("USER", "ADMIN");
+    }
+
+}
